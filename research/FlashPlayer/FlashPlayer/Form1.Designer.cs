eb39dfeb6bd510bed5a5642d9d610e040dead2d0
ductri-PC\ductri <tri.dinh@gosmartlog.com>
2018-07-21 13:28:01 +0700

    STMS-445

---
+++ research/FlashPlayer/FlashPlayer/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace FlashPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.playToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuPlay = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuPause = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuShowOnTop = new System.Windows.Forms.ToolStripMenuItem();
+            this.axShockwaveFlash1 = new AxShockwaveFlashObjects.AxShockwaveFlash();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.playToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(428, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuOpen});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // mnuOpen
+            // 
+            this.mnuOpen.Name = "mnuOpen";
+            this.mnuOpen.Size = new System.Drawing.Size(152, 22);
+            this.mnuOpen.Text = "Open";
+            this.mnuOpen.Click += new System.EventHandler(this.mnuOpen_Click);
+            // 
+            // playToolStripMenuItem
+            // 
+            this.playToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuPlay,
+            this.mnuPause,
+            this.mnuShowOnTop});
+            this.playToolStripMenuItem.Name = "playToolStripMenuItem";
+            this.playToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.playToolStripMenuItem.Text = "Control";
+            // 
+            // mnuPlay
+            // 
+            this.mnuPlay.Name = "mnuPlay";
+            this.mnuPlay.Size = new System.Drawing.Size(152, 22);
+            this.mnuPlay.Text = "Play";
+            this.mnuPlay.Click += new System.EventHandler(this.mnuPlay_Click);
+            // 
+            // mnuPause
+            // 
+            this.mnuPause.Name = "mnuPause";
+            this.mnuPause.Size = new System.Drawing.Size(152, 22);
+            this.mnuPause.Text = "Pause";
+            this.mnuPause.Click += new System.EventHandler(this.mnuPause_Click);
+            // 
+            // mnuShowOnTop
+            // 
+            this.mnuShowOnTop.Name = "mnuShowOnTop";
+            this.mnuShowOnTop.Size = new System.Drawing.Size(152, 22);
+            this.mnuShowOnTop.Text = "Show on top";
+            this.mnuShowOnTop.Click += new System.EventHandler(this.mnuShowOnTop_Click);
+            // 
+            // axShockwaveFlash1
+            // 
+            this.axShockwaveFlash1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.axShockwaveFlash1.Enabled = true;
+            this.axShockwaveFlash1.Location = new System.Drawing.Point(0, 27);
+            this.axShockwaveFlash1.Name = "axShockwaveFlash1";
+            this.axShockwaveFlash1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axShockwaveFlash1.OcxState")));
+            this.axShockwaveFlash1.Size = new System.Drawing.Size(428, 329);
+            this.axShockwaveFlash1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 357);
+            this.Controls.Add(this.axShockwaveFlash1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Flash player";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mnuOpen;
+        private System.Windows.Forms.ToolStripMenuItem playToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mnuPlay;
+        private System.Windows.Forms.ToolStripMenuItem mnuPause;
+        private System.Windows.Forms.ToolStripMenuItem mnuShowOnTop;
+        private AxShockwaveFlashObjects.AxShockwaveFlash axShockwaveFlash1;
+    }
+}
+
---
+++ research/FlashPlayer/FlashPlayer/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlashPlayer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mnuOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog op = new OpenFileDialog();
+                op.Filter = "Flash files|*.swf";
+                if (op.ShowDialog() == DialogResult.OK)
+                {
+                    axShockwaveFlash1.Movie = op.FileName;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void mnuPlay_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                axShockwaveFlash1.Playing = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void mnuPause_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                axShockwaveFlash1.Playing = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void mnuShowOnTop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (mnuShowOnTop.Checked)
+                {
+                    this.TopMost = false;
+                    mnuShowOnTop.Checked = false;
+                }
+                else
+                {
+                    this.TopMost = true;
+                    mnuShowOnTop.Checked = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
